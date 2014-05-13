0266b9f7997d912fc0af68baea106950ff811d8e
xia zhonglin <xia@xiadeiMac.local>
2014-05-13 10:35:03 +0800

    Initial Commit

---
+++ XCMonit_IpTests/XCMonit_IpTests.m
@@ -0,0 +1,34 @@
+//
+//  XCMonit_IpTests.m
+//  XCMonit_IpTests
+//
+//  Created by xia zhonglin  on 14-5-13.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface XCMonit_IpTests : XCTestCase
+
+@end
+
+@implementation XCMonit_IpTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
